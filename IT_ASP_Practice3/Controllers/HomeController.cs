dd7980ded51d178257b141b4ef39767e4e2b5eb9
Kataiii <99545244+Kataiii@users.noreply.github.com>
2022-04-01 21:19:34 +0400

    -create project -add page ControlPanel

---
+++ IT_ASP_Practice3/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IT_ASP_Practice3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult ControlPanel()
+        {
+            return View();
+        }
+    }
+}
