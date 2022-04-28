78197010f8644e3df45050ff8c5e28718e2cef2f
Kataiii <99545244+Kataiii@users.noreply.github.com>
2022-04-28 12:50:49 +0400

    add project practice6_3

---
+++ Practice6_3/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practice6_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Master()
+        {
+            return View();
+        }
+
+        public ActionResult FirstPage()
+        {
+            return View();
+        }
+
+        public ActionResult SecondPage()
+        {
+            return View();
+        }
+
+        public ActionResult Router_pages(string nextPage)
+        {
+            switch (nextPage)
+            {
+                case "first_page":
+                    return View("FirstPage");
+                case "second_page":
+                    return View("SecondPage");
+            }
+            return View("Master");
+        }
+    }
+}
---
+++ Practice6_3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Practice6_3.MvcApplication" Language="C#" %>
